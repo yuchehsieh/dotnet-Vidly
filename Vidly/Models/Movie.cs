b6cc1f2466068c1a14dbe47f5c9ac752a0749858
Murphy Hsieh <murphyhsieh@leandev.io>
2020-01-22 08:34:54 +0800

    Creating first dotnet project, create first model class

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+        public Movie()
+        {
+        }
+    }
+}
